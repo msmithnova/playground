9883c52e6ed42ca0446b8c28ba8cced7546f032d
Michael Smith <mikesmithnova@gmail.com>
2018-04-07 03:31:29 -0300

    Creaed FizzBuzz project and added tests

---
+++ C++/FizzBuzz/FizzBuzzTest/unittest1.cpp
@@ -0,0 +1,42 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "../FizzBuzz/FizzBuzz.cpp"
+#include <iostream>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace FizzBuzzTest
+{		
+	TEST_CLASS(UnitTest1)
+	{
+		std::stringstream ss;
+	public:
+		TEST_METHOD_INITIALIZE(RedirectCout) {
+			std::streambuf *coutbuf = std::cout.rdbuf(); //save old buf
+			std::cout.rdbuf(ss.rdbuf()); //redirect std::cout to ss
+		}
+
+		TEST_METHOD(TestMultipleOf3)
+		{
+			fizzBuzz1(2, 4);
+			Assert::AreEqual("2\nFizz\n4\n", ss.str().c_str());
+		}
+
+		TEST_METHOD(TestMultipleOf5)
+		{
+			fizzBuzz1(4, 7);
+			Assert::AreEqual("4\nBuzz\nFizz\n7\n", ss.str().c_str());
+		}
+
+		TEST_METHOD(TestMultipleOf3And5)
+		{
+			fizzBuzz1(14, 16);
+			Assert::AreEqual("14\nFizzBuzz\n16\n", ss.str().c_str());
+		}
+
+		TEST_METHOD_CLEANUP(RestoreCout) {
+			std::cout.rdbuf(ss.rdbuf()); //restore old buf
+		}
+
+	};
+}
